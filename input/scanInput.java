329a1b31938b60819df2a8dd878ae385808e90d5
Irfan Dwi Prasetyo <keepant@gmail.com>
2017-10-06 10:16:50 +0700

    input using scanner

---
+++ input/scanInput.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class scanInput{
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        String nama;
+        int umur;
+        double tinggi;
+        char jenkel;
+        boolean status;
+
+        System.out.print("Masukkan nama: ");
+        nama = input.nextLine();
+        System.out.print("Masukkan umur: ");
+        umur = input.nextInt();
+        System.out.print("Masukkan tinggi badan: ");
+        tinggi = input.nextDouble();
+        System.out.print("Masukkan jenis kelamin (L/P): ");
+        jenkel = input.next().charAt(0);
+     //   System.out.print("Status: ");
+       // status = input.nextBoolean();
+        System.out.println("-------------------------------------------OUTPUT-------------------------------------------------------");
+        System.out.println("Nama: "+nama+"\nUmur: "+umur+"\nTinggi badan: "+tinggi+"\nJenis Kelmin: "+jenkel+"\nstatus: "/*+status*/);
+    }
+}

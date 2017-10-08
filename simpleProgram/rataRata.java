719670eda4a3ff3b2a1742d08810286f494560d9
Irfan Dwi Prasetyo <keepant@gmail.com>
2017-10-08 20:35:13 +0700

    adding exception

--- simpleProgram/rataRata.java
+++ simpleProgram/rataRata.java
@@ -1,21 +1,26 @@
 import java.io.*;
 
+
 public class rataRata{
     public static void main(String[] args) throws Exception {
         BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
         int bil1, bil2, bil3;
         double hasil;
 
-        System.out.print("Masukkan bilangan pertama: ");
-        bil1 = Integer.parseInt(input.readLine());
-        System.out.print("Masukkan bilangan kedua: ");
-        bil2 = Integer.parseInt(input.readLine());
-        System.out.print("Masukkan bilangan ketiga: ");
-        bil3 = Integer.parseInt(input.readLine());
+        try{
+            System.out.print("Masukkan bilangan pertama: ");
+            bil1 = Integer.parseInt(input.readLine());
+            System.out.print("Masukkan bilangan kedua: ");
+            bil2 = Integer.parseInt(input.readLine());
+            System.out.print("Masukkan bilangan ketiga: ");
+            bil3 = Integer.parseInt(input.readLine());
+            
+            hasil=(double)(bil1+bil2+bil3)/3;
 
-        hasil=(double)(bil1+bil2+bil3)/3;
-
-        System.out.println("Rata-rata dari ketiga bilangan tersebut: "+hasil);
-
+            System.out.println("Rata-rata dari ketiga bilangan tersebut: "+hasil);
+        } catch(NumberFormatException e){
+            System.out.println("Inputan harus bilangan bulat!");
+        }
+        
     }
 }

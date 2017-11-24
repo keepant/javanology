a569c7384670e41603d0af46278728fb562ab854
Irfan Dwi Prasetyo <keepant@gmail.com>
2017-11-24 10:58:38 +0700

    arithmatic line

--- loop/deret.java
+++ loop/deret.java
@@ -4,13 +4,24 @@ import java.util.Scanner;
 public class deret{
     public static void main(String[] args) throws Exception{
         BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
-        int nilai;
+       /*  int nilai;
         int hasil=0;
         System.out.print("Masukkan nilai: ");
         nilai = Integer.parseInt(input.readLine());
         for (int i=1; i<=nilai; i++){
             hasil += i;
         }
-            System.out.print("Jumlah bilangan asli dari "+nilai+" pertama adalah "+hasil);
+            System.out.print("Jumlah bilangan asli dari "+nilai+" pertama adalah "+hasil); */
+        
+        int i, n, Sn;
+        Sn=0;
+
+        System.out.print("Masukkan nilai n: ");
+        n=Integer.parseInt(input.readLine());
+        i=1;
+        while(i<=n){
+            Sn = Sn+i;
+        i++;}
+        System.out.println("Jadi Jumlah dari Deret N: "+Sn);
     }
 }

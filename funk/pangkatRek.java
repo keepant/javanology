fae7ac78293c16dd2eb6f02089e4666452e36b5e
Irfan Dwi Prasetyo <keepant@gmail.com>
2018-01-07 22:24:44 +0700

    using recursive

--- funk/pangkatRek.java
+++ funk/pangkatRek.java
@@ -3,20 +3,23 @@ import java.io.*;
 public class pangkatRek{
     public static void main(String[] args) throws Exception{
         BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
-        int angka=0, pangkat=0;
+        int angka, pangkat, hasil;
+
+        System.out.print("Masukkan angka: ");
+        angka = Integer.parseInt(input.readLine());
+        System.out.print("Masukkan pangkat: ");
+        pangkat = Integer.parseInt(input.readLine());
 
         System.out.println("---Menghitung x pangkat y menggunakan fungsi return tanpa rekursi---");
         pangkatReturn(angka, pangkat);
+        System.out.println("---------------------------------------------------------------------");
+        System.out.println("---Menghitung x pangkat y menggunakan fungsi return dengan rekursi---");
+        System.out.println("Hasil dari "+angka+"^"+pangkat+" adalah "+pangkatRekursif(angka, pangkat));
+
     }
 
-    public static int pangkatReturn(int x, int y) throws Exception{
-        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
-
+    public static int pangkatReturn(int x, int y){
         int hasil, i;
-        System.out.print("Masukkan angka: ");
-        x = Integer.parseInt(input.readLine());
-        System.out.print("Masukkan pangkat: ");
-        y = Integer.parseInt(input.readLine());
 
         hasil=x;
         for(i=1;i<y;i++){
@@ -26,5 +29,17 @@ public class pangkatRek{
         System.out.println("Hasil dari "+x+"^"+y+" adalah "+hasil);
 
         return hasil;
+    }
+
+    public static int pangkatRekursif(int x, int y){
+        int hasil=0;
+
+        if(y==1){
+            hasil = x;
+        } else {
+            hasil = x * pangkatRekursif(x, y-1);
+        }
+        return hasil;
+
     }
 }
